3061d27d71c423883f1d15df6d9bc75ab1bcd258
CatSheng <cs1360@scarletmail.rutgers.edu>
2021-09-12 19:05:42 -0400

    Save Scene 8:
    In VS adding code to tell the game to destroy a basket when an apple is missed in ApplePicker.cs

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -10,10 +11,12 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for(int i=0; i<numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
@@ -23,6 +26,8 @@ public class ApplePicker : MonoBehaviour
             pos.y = basketBottomY + (basketSpacingY * i);
 
             tBasketGO.transform.position = pos;
+
+            basketList.Add(tBasketGO);
         }
     }
 
@@ -33,8 +38,19 @@ public class ApplePicker : MonoBehaviour
 
         foreach (GameObject tGO in tAppleArray)
         {
-            AppleDestroyed(tGO);
+            Destroy(tGO);
         }
+
+        //Destroy one of the baskets
+        //Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+
+        //Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+
+        //Remove the Basket from the list and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 
     // Update is called once per frame
