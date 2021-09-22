ad0e55e1792d3c7acc69e099ec55eec60aefb2fe
CatSheng <cs1360@scarletmail.rutgers.edu>
2021-09-22 15:32:34 -0400

    Save Scene 4:
    Gave the Slingshot a halo effect. Wrote code to show when the slingshot is active. Also wrote code for the game to detect when the mouse is close enough to the Slingshot to interact with it and to instantiate a projectile when the user clicks the mouse near the slingshot. Assigned the projectile to the slingshot.

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //fields set in the Unity Inspector pane
+    [Header("Set in Inspector")]
+
+    public GameObject prefabProjectile;
+
+    //fields set dynamically
+    [Header("Set Dynamically")]
+
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.FindChild("LaunchPoint");
+
+        launchPoint = launchPointTrans.gameObject;
+
+        launchPoint.SetActive(false);
+
+        launchPos = launchPointTrans.position;
+    }
+
+
     void OnMouseEnter()
     {
-        print("Slingshot: OnMouseEnter()");
+        //print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+    
     }
+
 
     // Update is called once per frame
     void OnMouseExit()
     {
-        print("Slingshot: OnMouseExit()");
+        //print("Slingshot: OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+
+        // Instantiate a Projectile
+        projectile = Instantiate(prefabProjectile) as GameObject;
+
+        //Start it at the launchPoint
+        projectile.transform.position = launchPos;
+
+        // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
